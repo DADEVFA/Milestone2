7a2f9e20d614fe8d214e062e85d702262fd8b07e
DADEVFA <davidfarah193@gmail.com>
2021-01-08 21:55:25 +0000

    Added full share functionallity

--- assets/js/shareSocials.js
+++ assets/js/shareSocials.js
@@ -1,39 +1,54 @@
- 
-document.addEventListener("DOMContentLoaded",function(){
- 
 
-//Getting page information
-let current_url = window.location.href;
-let currentPage_title = document.title;
- 
-//Targeting
-let facebook = document.getElementById("fb");
-let twitter = document.getElementById("tw");
-let whatsapp = document.getElementById("wts");
 
-//Facebook
-facebook.addEventListener('click',function(){
-	let fb_shareUrl = "https://www.facebook.com/sharer/sharer.php?u="+current_url;
-	let window_size = "width=300,height=400";
-	window.open(fb_shareUrl,"","menubar=no,resizeable=yes,scrollbars=yes,"+window_size);
-	return false;
+//social share functions
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    //Getting page information
+    let current_url = window.location.href;
+    let currentPage_title = document.title;
+
+    //Targeting
+    let facebook = document.getElementById("fb");
+    let twitter = document.getElementById("tw");
+    let whatsapp = document.getElementById("wts");
+    let copyurl = document.getElementById("cpy2");
+
+    //Facebook
+    facebook.addEventListener('click', function () {
+        let fb_shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + current_url;
+        let window_size = "width=300,height=400";
+        window.open(fb_shareUrl, "", "menubar=no,resizeable=yes,scrollbars=yes," + window_size);
+        return false;
+    });
+
+    //Twitter
+    twitter.addEventListener('click', function () {
+        let tw_shareUrl = "https://twitter.com/intent/tweet?text=" + currentPage_title + "&url=" + current_url;
+        let window_size = "width=300,height=400";
+        window.open(tw_shareUrl, "", "menubar=no,resizeable=yes,scrollbars=yes," + window_size);
+        return false;
+    });
+
+    //Whatapp
+    whatsapp.addEventListener('click', function () {
+        let wts_shareUrl = "whatsapp://send?text=" + current_url;
+        let window_size = "width=300,height=400";
+        window.open(wts_shareUrl, "", "menubar=no,resizeable=yes,scrollbars=yes," + window_size);
+        return false;
+    });
+
+
 });
 
-//Twitter
-twitter.addEventListener('click',function(){
-	let tw_shareUrl = "https://twitter.com/intent/tweet?text="+currentPage_title+"&url="+current_url;
-	let window_size = "width=300,height=400";
-	window.open(tw_shareUrl,"","menubar=no,resizeable=yes,scrollbars=yes,"+window_size);
-	return false;
-});
- 
-//Whatapp
-whatsapp.addEventListener('click',function(){
-	let wts_shareUrl = "whatsapp://send?text="+current_url;
-	let window_size = "width=300,height=400";
-	window.open(wts_shareUrl,"","menubar=no,resizeable=yes,scrollbars=yes,"+window_size);
-	return false;
-});
- 
-});
-
+    //Copy URL
+function urlShare() {
+    var sURL = window.location.href;
+    sTemp = "<input id=\"copy_to_Clipboard\" value=\"" + sURL + "\" />"
+    $("body").append(sTemp);
+    $("#copy_to_Clipboard").select();
+    document.execCommand("copy");
+    $("#copy_to_Clipboard").remove();
+     alert("URL is copied!");
+};
