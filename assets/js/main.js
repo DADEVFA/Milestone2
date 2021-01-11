551be6a6d6e69a5203d157e64a36d5202e9be4e6
DADEVFA <davidfarah193@gmail.com>
2021-01-11 15:04:57 +0000

    removed old js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,3 @@
-//Submit button changes to "Sent" when clicked. 
-function myFunction() {
-
-    var btn = document.getElementById("submit-button");
-
-    if (btn.value == "Submit") {
-        btn.value = "Sent"
-        btn.innerHTML = "Sent";
-        document.getElementById("submit-button").className = "sent-button";
-    } else {
-        btn.value = "Submit";
-        btn.innerHTML = "Submit";
-        document.getElementById("submit-button").className = "submit-button";
-    }
-}
-
 
 //Terms button Collapses with content and changes to "Close Terms" 
 $(document).ready(function () {
